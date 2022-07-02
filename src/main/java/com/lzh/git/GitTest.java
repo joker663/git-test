7b6c7af13a66c1450f21bee2eccce87d2050ecf1
joker <2564047684@qq.com>
2022-07-02 14:48:46 +0800

    first commit

---
+++ src/main/java/com/lzh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lzh.git;
+
+/**
+ * @Description: Git-Test
+ * @author: joker
+ * @Date: 2022/7/2 14:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git!");
+    }
+}
