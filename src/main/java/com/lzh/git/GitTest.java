2def058d7308c5c668d5867b771a4477c94cd24e
joker <2564047684@qq.com>
2022-07-02 15:55:38 +0800

    push commit2

--- src/main/java/com/lzh/git/GitTest.java
+++ src/main/java/com/lzh/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello,Git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
