f04d6ef6b21de994ea8f1cb13b2580958752ac9f
joker <2564047684@qq.com>
2022-07-02 14:54:48 +0800

    third commit

--- src/main/java/com/lzh/git/GitTest.java
+++ src/main/java/com/lzh/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git!");
         System.out.println("hello,Git2!");
+        System.out.println("hello,Git3!");
     }
 }
