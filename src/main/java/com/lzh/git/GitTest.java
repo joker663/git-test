bb864918ddc347b1098d224b5b6b301d3a1f895f
joker <2564047684@qq.com>
2022-07-02 14:53:28 +0800

    second commit

--- src/main/java/com/lzh/git/GitTest.java
+++ src/main/java/com/lzh/git/GitTest.java
@@ -8,5 +8,6 @@ package com.lzh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git!");
+        System.out.println("hello,Git2!");
     }
 }
