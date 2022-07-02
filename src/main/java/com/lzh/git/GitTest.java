cf398317d0fe4f222e4ac88d12659d01c137733f
joker <2564047684@qq.com>
2022-07-02 15:05:55 +0800

    hot-fix commit1

--- src/main/java/com/lzh/git/GitTest.java
+++ src/main/java/com/lzh/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello,Git!");
         System.out.println("hello,Git2!");
         System.out.println("hello,Git3!");
+        System.out.println("hello,Git4!");
     }
 }
