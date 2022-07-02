09607e974421a2e6f60241cf9c70024c614538e8
joker <2564047684@qq.com>
2022-07-02 15:11:06 +0800

    master commit2

--- src/main/java/com/lzh/git/GitTest.java
+++ src/main/java/com/lzh/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello,Git2!");
         System.out.println("hello,Git3!");
         System.out.println("hello,Git4!");
+        System.out.println("master test!");
     }
 }
